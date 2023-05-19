48eea69dfc6da7dce51ade98c909dc880219c45a
hongdahyeon <hyeon8287@gmail.com>
2023-05-19 10:27:33 +0900

    commit 1 - beginning

---
+++ src/main/java/yongtable/tabletest/TableTestApplication.java
@@ -0,0 +1,13 @@
+package yongtable.tabletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableTestApplication.class, args);
+	}
+
+}
