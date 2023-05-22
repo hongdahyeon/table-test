07b77a68212fb9eb3e1f40daf32e599b8cc6f07c
hongdahyeon <hyeon8287@gmail.com>
2023-05-22 14:52:05 +0900

    commit 29 - 파일 테이블 생성 (파일 그룹&파일 테이블 합쳐서 1개 테이블로 생성)

---
+++ src/main/java/yongtable/tabletest/domain/file_group/FileGroup.java
@@ -0,0 +1,29 @@
+package yongtable.tabletest.domain.file_group;
+
+import yongtable.tabletest.global.base.BaseTimeEntity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "yong_file_group")
+public class FileGroup extends BaseTimeEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "yong_file_group_id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private FileGroup parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<FileGroup> child = new ArrayList<>();
+}
