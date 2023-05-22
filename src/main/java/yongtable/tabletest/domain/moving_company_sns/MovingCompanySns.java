652fcdfa9c272762cd52f7e4e9ca0115f03a62d3
hongdahyeon <hyeon8287@gmail.com>
2023-05-22 15:09:17 +0900

    commit 31 - 이사 업체 sns 테이블 생성

---
+++ src/main/java/yongtable/tabletest/domain/moving_company_sns/MovingCompanySns.java
@@ -0,0 +1,24 @@
+package yongtable.tabletest.domain.moving_company_sns;
+
+import yongtable.tabletest.domain.moving_company.MovingCompany;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "yong_moving_company_sns")
+public class MovingCompanySns {
+
+    @Id @GeneratedValue
+    @Column(name = "yong_moving_company_sns_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "yong_moving_company_id")
+    private MovingCompany movingCompany;
+
+    @Enumerated(EnumType.STRING)
+    private SnsType snsType;
+
+    @Column(name = "sns_url")
+    private String snsUrl;
+}
---
+++ src/main/java/yongtable/tabletest/domain/moving_company_sns/SnsType.java
@@ -0,0 +1,5 @@
+package yongtable.tabletest.domain.moving_company_sns;
+
+public enum SnsType {
+    INSTAGRAM, BLOG, KAKAOTALK
+}
